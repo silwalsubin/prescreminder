09b3891b7e6cb878954e37565b75f9025245940b
Subin Silwal <silwalsubin@gmail.com>
2021-11-07 20:24:53 -0600

    Set Up - Hello World Server API

---
+++ services/src/prescreminder.API/Controllers/HeartBeatController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prescreminder.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HeartBeatController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Is Alive";
+        }
+    }
+}
---
+++ services/src/prescreminder.API/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace prescreminder.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .Build()
+                .Run();
+        }
+    }
+}
