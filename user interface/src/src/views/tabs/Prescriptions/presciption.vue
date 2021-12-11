1d8c26973222e1ef39838d8748969ee878fe6dc0
Subin Silwal <silwalsubin@gmail.com>
2021-12-10 22:04:28 -0600

    edit delete enhancements

--- user interface/src/src/views/tabs/Prescriptions/presciption.vue
+++ user interface/src/src/views/tabs/Prescriptions/presciption.vue
@@ -9,32 +9,27 @@
       </ion-text>
     </div>
     <div class="prescription-actions" slot="end">
-      <div class="precription-actions-buttons">
-        <ion-button
-          fill="clear"
-          color="success"
-          @click="setOpen(true)"
-        >
-          <ion-icon :icon="createOutline" />
-        </ion-button>
-        <ion-button
-          fill="clear"
-          color="danger"
-          @click="handleDelete"
-        >
-          <ion-icon :icon="trashOutline" />
-        </ion-button>
-        <ion-modal
-          :is-open="isOpenRef"
-          @didDismiss="setOpen(false)"
-        >
-          <Modal 
-            :prescriptionId="prescription.prescriptionId"
-            @close="setOpen(false)"
-            :isVisible="isOpenRef"
-          />
-        </ion-modal>
-      </div>
+      <ion-button
+        fill="clear"
+        class="prescription-action-button"
+        color="primary"
+        @click="handleActionClick"
+      >
+        <ion-icon
+          size="large"
+          :icon="ellipsisHorizontalCircleSharp" 
+        />
+      </ion-button>
+      <ion-modal
+        :is-open="isOpenRef"
+        @didDismiss="setOpen(false)"
+      >
+        <Modal 
+          :prescriptionId="prescription.prescriptionId"
+          @close="setOpen(false)"
+          :isVisible="isOpenRef"
+        />
+      </ion-modal>
     </div>
   </form-item>
 </template>
@@ -45,12 +40,11 @@ import Modal from '@/components/add-edit-prescription-modal.vue'
 import { notifyAsync, NotificationType } from '@/toast-notifications'
 import PrescriptionViewModal from '@/store/view-models/prescription-view-modal'
 import {
-  createOutline,
-  trashOutline,
+  ellipsisHorizontalCircleSharp,
 } from 'ionicons/icons';
 
 import { useStore } from '@/store/store';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 import {
   IonButton,
@@ -58,6 +52,8 @@ import {
   IonIcon,
   IonLabel,
   IonText,
+  actionSheetController,
+  alertController,
 } from '@ionic/vue';
 
 export default {
@@ -77,18 +73,73 @@ export default {
     const store = useStore();
     const isOpenRef = ref(false);
     const setOpen = (state) => isOpenRef.value = state;
-    const handleDelete = async () => {
-      await store.dispatch('deletePrescription', props.prescription.prescriptionId);
-      const message = `${props.prescription.name} ${props.prescription.unitDose} deleted successfully`;
-      await notifyAsync(NotificationType.Success, message);
+
+    const itemLabel = computed(() => `${props.prescription.name} ${props.prescription.unitDose}`);
+
+    const handleActionClick = async () => {
+      const actionSheet = await actionSheetController.create({
+          header: `${itemLabel.value}`,
+          buttons: [
+            {
+              text: 'Edit',
+              handler: () => {
+                setOpen(true);
+              },
+            },
+            {
+              text: 'Delete',
+              role: 'destructive',
+              handler: async () => {
+                const alert = await alertController.create({
+                  header: 'Are you Sure?',
+                  message: `This will delete ${itemLabel.value} and all its associated data`,
+                  buttons: [
+                    {
+                      text: 'Cancel',
+                      role: 'cancel',
+                      handler: () => {
+                        // do nothing 
+                      },
+                    },
+                    {
+                      text: 'Continue',
+                      handler: async () => {
+                        await store.dispatch('deletePrescription', props.prescription.prescriptionId);
+                        const message = `${itemLabel.value} deleted successfully`;
+                        await notifyAsync(NotificationType.Success, message);
+                      },
+                    },
+                  ],
+                });
+                await alert.present();
+              },
+            },
+            {
+              text: 'Cancel',
+              role: 'cancel',
+              handler: () => {
+                // do nothing
+              },
+            },
+          ],
+        });
+        await actionSheet.present();
     }
     return {
+      itemLabel,
       isOpenRef, 
       setOpen,  
-      handleDelete,
-      createOutline,
-      trashOutline,
+      handleActionClick,
+      ellipsisHorizontalCircleSharp,
     }
   }
 }
 </script>
+
+<style scoped lang="scss">
+@import "@/styles/animations/blinking-animation.scss";
+.prescription-action-button {
+  margin-right: -13px;
+  animation: $blink-light;
+}
+</style>
